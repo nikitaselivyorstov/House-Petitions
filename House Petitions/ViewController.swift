17faa2a169bfa4ef3c535f6f8c3ac56551b3fff5
nikitaseliverstov <nikita77077@inbox.ru>
2018-05-18 03:20:50 +0300

    Initial Commit

---
+++ House Petitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  House Petitions
+//
+//  Created by Nikita Seliverstov  on 18.05.2018.
+//  Copyright © 2018 Nikita Seliverstov . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
